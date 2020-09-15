04218d014c1c6515fb6e316cc5c67a653102a343
Stricks1 <gmalheiross@gmail.com>
2020-09-14 21:22:47 -0300

    Fix: Changed App funtion to a const using arrow function

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Display from './Display';
 import ButtonPannel from './ButtonPannel';
 
-function App() {
-  return (
-    <div className="App">
-      <Display />
-      <ButtonPannel />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Display />
+    <ButtonPannel />
+  </div>
+);
 
 export default App;
